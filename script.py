877bcf6e0fcaff27028b8574858141c783eafff8
Zubair Abdullah <31964315+zsabdullah@users.noreply.github.com>
2023-11-29 19:41:17 -0600

    titles and desc in api call

--- script.py
+++ script.py
@@ -21,6 +21,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 def initialize():
     pinecone.init(
@@ -66,52 +67,81 @@ def create_index_from_file(file_path):
             index.append(cleaned_line)
     return index
 
+def find_name_by_number(courses, course_number):
+    for course in courses:
+        if course["Course Number"] == course_number:
+            return course["Course Name"]
+    return None
+
+def find_desc_by_number(courses, course_number):
+    for course in courses:
+        if course["Course Number"] == course_number:
+            return course["Description"]
+    return None
+
 def find_similar_courses(index, llm_response):
+    # Load course desc and title data
+    file_path = 'courses.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    courses = data['courses']
+
     vectorizer = TfidfVectorizer()
-    dict = {}
+    similarity_dict = {}
     for i in index:
         tfidf_matrix = vectorizer.fit_transform([llm_response, i])
         similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-        dict[similarity[0][0]] = i[20:23]
-        # print(similarity[0][0], i[20:23])
+        similarity_dict[similarity[0][0]] = i[20:23]
 
-    dict = sorted(dict.items(), reverse=True)
-    # print(dict)
-    max_value = max(item[0] for item in dict)
-    scaled_vals = [(item[0], item[1], (item[0] / max_value) * 100) for item in dict]
-    # print(scaled_vals)
+    sorted_similarities = sorted(similarity_dict.items(), reverse=True)
+    max_value = max(item[0] for item in sorted_similarities)
+    scaled_vals = [{"val": item[0], "course_number": item[1], "scaled_val": (item[0] / max_value) * 100, "course_name": "SWE", "course_desc": "A class."} for item in sorted_similarities]
 
-    scaled_vals = [{"val": i[0], "course_number": i[1], "scaled_val": i[2]} for i in scaled_vals]
+    for item in scaled_vals:
+        number = item["course_number"]
+        item["course_name"] = find_name_by_number(courses, number)
+        item["course_desc"] = find_desc_by_number(courses, number)
+
     json_string = json.dumps(scaled_vals, indent=4)
-    # print(json_string)
-
-    return(json_string)
+    return json_string
 
 @app.route('/process_user_input', methods=['POST'])
 def process_user_input():
-    # Assuming the user input is sent in the request body as JSON
-    data = request.json
-    user_input = data.get('user_input')
-    
+    # Load course data
+    file_path = 'courses.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    courses = data['courses']
+
+    user_input = request.json.get('user_input')
+
     llm_chain = initialize()
     llm_response = llm_chain.run(user_input)
 
     file_path = 'Data_Files/Catalog.txt'
     index = create_index_from_file(file_path)
 
+    # Calculate similarities
     vectorizer = TfidfVectorizer()
-    dict = {}
+    similarity_dict = {}
     for i in index:
         tfidf_matrix = vectorizer.fit_transform([llm_response, i])
         similarity = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
-        dict[similarity[0][0]] = i[20:23]
+        similarity_dict[similarity[0][0]] = i[20:23]
 
-    dict = sorted(dict.items(), reverse=True)
-    max_value = max(item[0] for item in dict)
-    scaled_vals = [(item[0], item[1], (item[0] / max_value) * 100) for item in dict]
+    # Sort and scale values
+    sorted_similarities = sorted(similarity_dict.items(), reverse=True)
+    max_value = max(item[0] for item in sorted_similarities)
+    scaled_vals = [{"val": item[0], "course_number": item[1], "scaled_val": (item[0] / max_value) * 100} for item in sorted_similarities]
 
-    scaled_vals = [{"val": i[0], "course_number": i[1], "scaled_val": i[2]} for i in scaled_vals]
+    # Add course name and description
+    for item in scaled_vals:
+        number = item["course_number"]
+        item["course_name"] = find_name_by_number(courses, number)
+        item["course_desc"] = find_desc_by_number(courses, number)
+
     return jsonify(scaled_vals)
+
 
 
 # def main():
@@ -124,7 +154,6 @@ def process_user_input():
 
 # main()
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
     print("Started")
