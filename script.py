8bc0e0460a9de2536d4c87a75e9b4a60258fa51e
Zubair Abdullah <31964315+zsabdullah@users.noreply.github.com>
2023-12-03 13:16:57 -0600

    new frontend

--- script.py
+++ script.py
@@ -15,13 +15,16 @@ from langchain.chains import (
     StuffDocumentsChain, LLMChain, ConversationalRetrievalChain
 )
 import json
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+import streamlit as st
+import plost
+import pandas as pd
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
 def initialize():
     pinecone.init(
@@ -35,11 +38,15 @@ def initialize():
 
     template = '''I'm trying to run cosine similarity on a user-generated text input against a number of course descriptions. To do this, I will need to convert the user-generated text input into a course description. Using your own knowledge of the content of computer science classes as well as the examples below, you will create the course description. Your description should be a little bit longer and more verbose than the examples.
 
-    Here are some examples of course descriptions:
+    Here are some examples of user inputs and relevant course descriptions:
+    User: "I'm interested in low-level languages"
+    Assistant: "Principles of embedded system architecture and programming; fundamentals and theoretical foundations of wireless communication systems; hands-on experiences of how an embedded system could be used to solve problems in biomedical engineering; projects on wireless sensors and imaging for medical devices. Programming language translation; functions and general organization of compiler design and interpreters; theoretical and implementation aspects of lexical scanners; parsing of context free languages; code generation and optimization; error recovery. Security principles; common security features and flaws in day-to-day embedded systems; security analysis, vulnerability exploits and security fixes for embedded systems."
 
-    Course Title: CSCE 411 Design and Analysis of Algorithms, Description: Credits 3.  3 Lecture Hours.       Study of computer algorithms for numeric and non-numeric problems; design paradigms; analysis of time and space requirements of algorithms; correctness of algorithms; NP-completeness and undecidability of problems. Prerequisite:  Grade of C or better in CSCE 221 and CSCE 222/ECEN 222; junior or senior classification or approval of instructor.
-    Course Title: CSCE 412 Cloud Computing, Description: Credits 3.  3 Lecture Hours.       Operating system and distributed systems fields that form the basis of cloud computing such as virtualization, key-value storage solutions, group membership, failure detection, peer to peer systems, datacenter networking, resource management and scalability; popular frameworks such as MapReduce and HDFS and case studies on failure determination. Prerequisite:  Grade of C or better in CSCE 315 or CSCE 331.
-    Course Title: CSCE 413 Software Security, Description: Credits 3.  3 Lecture Hours.       Basic principles of design and implementation of defect-free software, code reviews including tool-assisted review by static and dynamic analysis, risk analysis and management and methods for software security testing. Prerequisites:  Grade of C or better in CSCE 315 or CSCE 331; or approval of instructor.
+    User: "I like Big Data and want to work in the cloud one day"
+    Assistant: "Operating system and distributed systems fields that form the basis of cloud computing such as virtualization, key-value storage solutions, group membership, failure detection, peer to peer systems, datacenter networking, resource management and scalability; popular frameworks such as MapReduce and HDFS and case studies on failure determination. Theoretical foundations, algorithms and methods of data analytics for cybersecurity; study of data analytics including cluster analysis, supervised machine learning, anomaly detection, and visualization applied to cyber attacks, anomaly detection, vulnerability analysis, strategic manipulation, propaganda and other topics. Representation of, storage of and access to very large multimedia document collections; fundamental data structures and algorithms of current information storage and retrieval systems and relates various techniques to design and evaluation of complete retrieval systems."
+
+    User: "Machine learning"
+    Assistant: "Theoretical foundations of machine learning, pattern recognition and generating predictive models and classifiers from data; includes methods for supervised and unsupervised learning (decision trees, linear discriminants, neural networks, Gaussian models, non-parametric models, clustering, dimensionality reduction, deep learning), optimization procedures and statistical inference. Theoretical foundations, algorithms and methods of data analytics for cybersecurity; study of data analytics including cluster analysis, supervised machine learning, anomaly detection, and visualization applied to cyber attacks, anomaly detection, vulnerability analysis, strategic manipulation, propaganda and other topics. Fundamental concepts and techniques of intelligent systems; representation and interpretation of knowledge on a computer; search strategies and control; active research areas and applications such as notational systems, natural language understanding, vision systems, planning algorithms, intelligent agents and expert systems."
 
     Convert the following user-generated string into a course description. Do not include anything other than the description in your response. Here is the user-generated string:
     {user}'''
@@ -112,7 +119,7 @@ def find_similar_courses(index, llm_response):
     json_string = json.dumps(scaled_vals, indent=4)
     return json_string
 
-@app.route('/process_user_input', methods=['POST'])
+# @app.route('/process_user_input', methods=['POST'])
 def process_user_input():
     # Load course data from JSON
     file_path = 'courses.json'
@@ -120,10 +127,12 @@ def process_user_input():
         data = json.load(file)
     courses = data['courses']
 
-    user_input = request.json.get('user_input')
+    # user_input = request.json.get('user_input')
+    user_input = st.text_input("What are your computer science interests or career goals?")
 
     llm_chain = initialize()
     llm_response = llm_chain.run(user_input)
+    # llm_response = "Aesthetic and technical aspects of computer game development, including game mechanics, story development, content creation and game programming; includes game design, interface design, 3D modeling and animation, graphics algorithms, shader programming and artificial intelligence; group project includes the design and development of a game from start to finish"
 
     # Calculate similarities
     vectorizer = TfidfVectorizer()
@@ -141,6 +150,7 @@ def process_user_input():
 
     # Add course name and description
     for item in scaled_vals:
+        print(item["course_number"], "HELP")
         number = item["course_number"]
         for course in courses:
             if course["Course Number"] == number:
@@ -149,7 +159,7 @@ def process_user_input():
                 item["difficulty"] = course["Difficulty"]
                 break
 
-    return jsonify(scaled_vals)
+    return json.dumps(scaled_vals, indent=4)
 
 
 
@@ -164,5 +174,46 @@ def process_user_input():
 # main()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
     print("Started")
+
+    st.title("CourseCrafter")
+    
+    # llm_chain = initialize()
+    # llm_response = run(llm_chain=llm_chain)
+    # file_path = 'Data_Files/Catalog.txt'
+    # index = create_index_from_file(file_path)
+    # json_string = find_similar_courses(index=index, llm_response=llm_response)
+    # data = json.loads(json_string)
+    # print(json_string)
+    # df = pd.DataFrame(data)
+
+    json_data = process_user_input()
+    if isinstance(json_data, str):
+        data = json.loads(json_data)
+        print("1")
+    else:
+        data = json_data
+        print("2")
+    df = pd.DataFrame(data)
+    # df['difficulty'] = pd.to_numeric(df['difficulty'])
+    # df['scaled_val'] = pd.to_numeric(df['scaled_val'])
+    print(df)
+    print(len(df))
+    print(json_data)
+
+    plost.scatter_chart(
+        df,
+        x="difficulty",
+        y="scaled_val",
+        opacity="course_number",
+        size="course_number"
+    )
+    
+    
+    for index, row in df.iterrows():
+        if index >= 5:
+            break
+        st.write("CSCE", row["course_number"], "-", row["course_name"], "-", row["course_desc"])
+        st.write("Difficulty:", row["difficulty"], "Relevance:", str(round(row["scaled_val"], 2)))
+        st.write()
